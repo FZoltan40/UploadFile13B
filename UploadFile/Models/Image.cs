73a6640205831ea3f4737c0af4f09ae3e2baf13e
FZoltan40 <farkasz@kkszki.hu>
2026-02-16 08:05:34 +0100

    Sacffold is ready.

---
+++ UploadFile/Models/Image.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UploadFile.Models;
+
+public partial class Image
+{
+    public int Id { get; set; }
+
+    public byte[] ImageData { get; set; } = null!;
+
+    public string FileName { get; set; } = null!;
+
+    public string ContentType { get; set; } = null!;
+}
---
+++ UploadFile/Models/PicturesContext.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace UploadFile.Models;
+
+public partial class PicturesContext : DbContext
+{
+    public PicturesContext()
+    {
+    }
+
+    public PicturesContext(DbContextOptions<PicturesContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Image> Images { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseMySQL("server=localhost;database=pictures;user=root;password=");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Image>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.ToTable("images");
+
+            entity.Property(e => e.Id).HasColumnType("int(11)");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
